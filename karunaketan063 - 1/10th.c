0b5d5e31f149107d5e83f06563f80ae145d9b2ac
karuna ketan <81628692+karunaket@users.noreply.github.com>
2022-11-28 12:09:47 +0530

    Add files via upload

---
+++ karunaketan063 - 1/10th.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main()
+{
+	int Tamil = 100, English = 99, Maths = 98;
+	
+	float avg = (Tamil + English + Maths)/3;
+	
+	printf("The avegare = %f", avg);
+	
+	return 0;
+}
--- karunaketan063 - 1/9th.c
+++ karunaketan063 - 1/9th.c
@@ -8,6 +8,18 @@ int main()
 	printf("The sum of x and y = %d \n", z);   // Using variable
 	
 	printf("The sum of x and y = %d \n", x + y);  // Without using variable
+	
+	printf("The difference of x and y = %d \n", x - y);
+	
+	printf("The multiplication of x and y = %d \n", x * y);
+	
+	printf("The division of x and y = %d \n", x / y);  // Quotient
+	
+	printf("The division of x and y = %d \n", x % y);  // Remainder
+	
+	printf("The value = %d \n", ++x+y);  // = 1 + x + y = 8
+	
+	printf("The value = %d \n", ++x-y);  // = 1 + x - y = 
 		
 	return 0;
 }
