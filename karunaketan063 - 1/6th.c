7f101e4a59ac34617299b58663793117052a2b4d
karuna ketan <81628692+karunaket@users.noreply.github.com>
2022-11-28 11:48:45 +0530

    Add files via upload

--- karunaketan063 - 1/6th.c
+++ karunaketan063 - 1/6th.c
@@ -5,7 +5,8 @@ int main()
 	char x = 'a';
 	printf("The given character is %c. \n", x);
 	
-	char y = 'abc';
+	char y = 'abc';  /* only one character get's printed and if you 
+						want to print word then string is used */
 	printf("The given character is %c.", y);
 	
 	return 0;
