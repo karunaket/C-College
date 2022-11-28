21afacb8dcab9762b63fb61d6d0ec2533f0fe177
karuna ketan <81628692+karunaket@users.noreply.github.com>
2022-11-28 12:23:58 +0530

    Add files via upload

---
+++ karunaketan063 - 2/10th.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main()
+{
+	/* int Tamil = 100, English = 99, Maths = 98;
+	
+	float avg = (Tamil + English + Maths)/3;
+	
+	printf("The avegare = %f", avg); */
+	
+	// cut-off mark
+	
+	int phy = 100, chem = 99, maths =98;
+	
+	float cut = ((phy+chem)/2)+maths;
+	
+	printf("Cut-off = %f", cut);
+	
+	return 0;
+}
