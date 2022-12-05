47290186d1863236a55b2a82df19e0a51e9317dc
karuna ketan <81628692+karunaket@users.noreply.github.com>
2022-12-05 10:01:51 +0530

    Add files via upload

---
+++ karunaketan063 - 2/11th.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main()
+{
+	int maths, physics, chemistry, bio;
+	float engg_cutoff, med_cutoff;
+	
+	printf("Enter the physics mark:");
+	scanf("%d",&physics);
+	
+	printf("Enter the maths mark:");
+	scanf("%d",&maths);
+	
+	printf("Enter the chemistry mark:");
+	scanf("%d",&chemistry);
+	
+	printf("Enter the biology mark:");
+	scanf("%d",&bio);
+	
+	engg_cutoff = (maths + chemistry + physics)/3;
+	med_cutoff = (bio + chemistry + physics)/3;
+	
+	printf("\n \n Engineering cut-off is %.2f", engg_cutoff);
+	printf("\n Medical dutoff is %.2f", med_cutoff);
+	
+	return 0;
+}
